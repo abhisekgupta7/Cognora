88ed8ce872aead71117d8d38c3ba6d69cd859da2
Abhisek Gupta <abhisekgupta.dev@gmail.com>
2026-06-21 14:37:13 +0545

    console removed

--- cognora-frontend/components/SessionWrapper.tsx
+++ cognora-frontend/components/SessionWrapper.tsx
@@ -3,7 +3,6 @@ import { SessionProvider } from "next-auth/react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  console.log("CLERK KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
   return (
     <SessionProvider>
       <ClerkProvider
