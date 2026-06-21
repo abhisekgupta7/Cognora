608ce74a46183ceef3cfb985eb572a7aded8f8a4
Abhisek Gupta <abhisekgupta.dev@gmail.com>
2026-06-21 14:20:51 +0545

    clerk console msg added

--- cognora-frontend/components/SessionWrapper.tsx
+++ cognora-frontend/components/SessionWrapper.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import { SessionProvider } from "next-auth/react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  console.log("CLERK KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
   return (
     <SessionProvider>
       <ClerkProvider
