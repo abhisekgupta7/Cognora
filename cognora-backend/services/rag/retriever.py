19473098a6731273fe5212b3d1ebe635f3aeab82
Abhisek Gupta <abhisekgupta.dev@gmail.com>
2026-06-21 15:38:36 +0545

    retreiver updated

--- cognora-backend/services/rag/retriever.py
+++ cognora-backend/services/rag/retriever.py
@@ -1,18 +1,16 @@
 from db.connection import get_conn
+
 class Retriever:
-    def __init__(self):
-        self.conn = get_conn()
-        self.cursor = self.conn.cursor()
 
     def search(self, query_vector: list[float], lesson_id: str, course_id: str, org_id: str, top_k: int = 5):
         query = """
             SELECT chunk_text, embedding
             FROM chunks
             WHERE lesson_id = %s AND course_id = %s AND org_id = %s
-            ORDER BY embedding <-> %s ::vector
+            ORDER BY embedding <-> %s::vector
             LIMIT %s
         """
-
-        self.cursor.execute(query, (lesson_id, course_id, org_id, query_vector, top_k))
-        results = self.cursor.fetchall()
-        return results  
+        with get_conn() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query, (lesson_id, course_id, org_id, query_vector, top_k))
+                return cursor.fetchall()
